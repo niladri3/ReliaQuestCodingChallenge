9dcaa57b6b880ee5658f1f90639d0c2a089be657
Niladri Chanda <nchanda@qualys.com>
2025-08-06 02:57:05 +0530

    Added all the client methods that will interact with rqserver api like : getEmployee, getEmployeeById, deleteEmployeeByName,createEmployee

--- api/src/main/java/com/reliaquest/api/client/RQEmployeeClientImpl.java
+++ api/src/main/java/com/reliaquest/api/client/RQEmployeeClientImpl.java
@@ -1,15 +1,26 @@
 package com.reliaquest.api.client;
 
+import com.reliaquest.api.exception.ClientException;
 import com.reliaquest.api.model.EmployeeResponse;
 import com.reliaquest.api.model.rqserver.RQEmployeeCreateRequest;
+import com.reliaquest.api.model.rqserver.RQEmployeeData;
 import com.reliaquest.api.model.rqserver.RQEmployeeDeleteRequest;
+import com.reliaquest.api.model.rqserver.RQEmployeeResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.retry.annotation.Backoff;
 import org.springframework.retry.annotation.EnableRetry;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.ResourceAccessException;
 import org.springframework.web.client.RestClient;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @Slf4j
@@ -23,23 +34,96 @@ public class RQEmployeeClientImpl implements RQEmployeeClient {
         this.restClient = restClient;
     }
 
-    @Override
+    @Retryable(
+            retryFor = ResourceAccessException.class,
+            maxAttempts = 3,
+            backoff = @Backoff(delay = 2000)
+    )
     public List<EmployeeResponse> getEmployees() {
-        return List.of();
+        RQEmployeeResponse<List<RQEmployeeData>> response = restClient.get()
+                .retrieve()
+                .body(new ParameterizedTypeReference<>() {
+                });
+
+        List<RQEmployeeData> rqEmployeeList = Optional.ofNullable(response)
+                .map(RQEmployeeResponse::getData)
+                .orElseGet(Collections::emptyList);
+
+        log.info("Retrieved {} employees.", rqEmployeeList.size());
+        return EmployeeResponse.from(rqEmployeeList);
     }
 
-    @Override
+
+    @Retryable(
+            retryFor = ResourceAccessException.class,
+            maxAttempts = 3,
+            backoff = @Backoff(delay = 2000)
+    )
     public EmployeeResponse getEmployeeById(String id) {
-        return null;
+        RQEmployeeResponse<RQEmployeeData> response = restClient.get()
+                .uri("/{id}", id)
+                .retrieve()
+                .body(new ParameterizedTypeReference<>() {
+                });
+
+        RQEmployeeData rqEmployeeData = Optional.ofNullable(response)
+                .map(RQEmployeeResponse::getData)
+                .orElseThrow(() -> new ClientException(HttpStatus.NOT_FOUND,
+                        "Failed to get data for the given employee Id : " + id));
+
+        log.info("Successfully fetched employee data: {} for given id: {}.", rqEmployeeData, id);
+        return EmployeeResponse.from(rqEmployeeData);
     }
 
-    @Override
+
+    @Retryable(
+            retryFor = ResourceAccessException.class,
+            maxAttempts = 3,
+            backoff = @Backoff(delay = 2000)
+    )
     public String deleteEmployeeByName(RQEmployeeDeleteRequest rqEmployeeDeleteRequest) {
-        return "";
+
+        RQEmployeeResponse<Boolean> response = restClient.method(HttpMethod.DELETE)
+                .body(rqEmployeeDeleteRequest)
+                .retrieve()
+                .body(new ParameterizedTypeReference<>() {
+                });
+
+        boolean isDeleted = Optional.ofNullable(response)
+                .map(RQEmployeeResponse::getData)
+                .orElseThrow(() -> new ClientException(HttpStatus.UNPROCESSABLE_ENTITY,
+                        "Failed to delete employee: missing or incomplete Employee API response."));
+
+        // In most cases, `isDeleted` will not be false, because EmployeeService always validates the ID and name
+        // before invoking this method. This check is purely defensive—to catch any unexpected conditions.
+        if (!isDeleted) {
+            throw new ClientException(HttpStatus.NOT_FOUND, "Data does not exist with given name : "
+                    + rqEmployeeDeleteRequest.getName());
+        }
+
+        log.info("Successfully deleted employee: {}", rqEmployeeDeleteRequest.getName());
+        return rqEmployeeDeleteRequest.getName();
     }
 
-    @Override
+
+    @Retryable(
+            retryFor = ResourceAccessException.class,
+            maxAttempts = 3,
+            backoff = @Backoff(delay = 2000)
+    )
     public EmployeeResponse createEmployee(RQEmployeeCreateRequest rqEmployeeCreateRequest) {
-        return null;
+        RQEmployeeResponse<RQEmployeeData> response = restClient.post()
+                .body(rqEmployeeCreateRequest)
+                .retrieve()
+                .body(new ParameterizedTypeReference<>() {
+                });
+
+        RQEmployeeData RQEmployeeData = Optional.ofNullable(response)
+                .map(RQEmployeeResponse::getData)
+                .orElseThrow(() -> new ClientException(HttpStatus.UNPROCESSABLE_ENTITY,
+                        "Failed to create employee: missing or incomplete Employee API response."));
+
+        log.info("Successfully created employee: {}", RQEmployeeData);
+        return EmployeeResponse.from(RQEmployeeData);
     }
 }
